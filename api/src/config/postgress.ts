6a179fc1ef07980e42ce8357a5591b265489f3d0
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2025-06-10 07:53:02 +0100

    Update postgress.ts

--- api/src/config/postgress.ts
+++ api/src/config/postgress.ts
@@ -1,15 +1,28 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
 
-// Load environment variables
-dotenv.config();
+import { PrismaClient } from '@prisma/client';
 
-const pool = new Pool({
-  user: process.env.DB_USER,       // Database username
-  host: process.env.DB_HOST,       // Database host
-  database: process.env.DB_NAME,   // Database name
-  password: process.env.DB_PASSWORD, // Database password
-  port: Number(process.env.DB_PORT) || 5432, // Database port (default: 5432)
-});
+const prisma = new PrismaClient();
 
-export default pool;
+export default prisma;
+
+
+
+
+
+
+
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+
+// // Load environment variables
+// dotenv.config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,       // Database username
+//   host: process.env.DB_HOST,       // Database host
+//   database: process.env.DB_NAME,   // Database name
+//   password: process.env.DB_PASSWORD, // Database password
+//   port: Number(process.env.DB_PORT) || 5432, // Database port (default: 5432)
+// });
+
+// export default pool;
